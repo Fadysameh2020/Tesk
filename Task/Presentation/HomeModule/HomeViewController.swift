84ebe0d529943680b6cd103d1682e58a960574ab
Fadysameh2020 <fadysameh10@gmail.com>
2024-06-24 20:34:11 +0300

    first commit

---
+++ Task/Presentation/HomeModule/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewController.swift
+//  Task
+//
+//  Created by Fady Sameh on 6/24/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        navigationItem.title = "Technical support"
+
+    }
+
+
+    @IBAction func goToOtherScreens(_ sender: Any) {
+        
+    }
+
+
+}
